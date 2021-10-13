7420da950bd829765f97fd9c1382fec2e60626b4
skyelves <1600012805@pku.edu.cn>
2021-10-12 22:07:50 -0400

    write_dram_latency

--- latency.cpp
+++ latency.cpp
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <pthread.h>
 #include <sys/timeb.h>
+#include "rng/rng.h"
 
 using namespace std;
 using namespace std::chrono;
@@ -21,6 +22,7 @@ using namespace std::chrono;
 #define MAP_SYNC 0x080000 /* perform synchronous page faults for the mapping */
 #define MAP_SHARED_VALIDATE 0x03    /* share + validate extension flags */
 #define ALLOC_SIZE ((size_t)4<<30) // 4GB
+#define CACHELINESIZE (64)
 #define NUM_THREADS 5
 
 #define Time_BODY(condition, name, func)                                                        \
@@ -35,24 +37,32 @@ using namespace std::chrono;
         cout << throughPut << ", " << endl;                                                     \
     }
 
-pthread_t tids[NUM_THREADS];
+inline void mfence(void) {
+    asm volatile("mfence":: :"memory");
+}
+
+inline void clflush(char *data, size_t len) {
+    volatile char *ptr = (char *) ((unsigned long) data & (~(CACHELINESIZE - 1)));
+    mfence();
+    for (; ptr < data + len; ptr += CACHELINESIZE) {
+        asm volatile("clflush %0" : "+m" (*(volatile char *) ptr));
+    }
+    mfence();
+}
 
 string pm_file = "/media/pmem0/ke/pm_bench_pm";
 string ssd_file = "/home/ke_wang/test/pm_bench_ssd";
+uint64_t object_size = 64;
 
-char *dram_addr = NULL, *pm_addr = NULL, *ssd_addr = NULL;
+pthread_t tids[NUM_THREADS];
 
-uint64_t size = 1 << 30; //1GB
-uint64_t cache_line_size = 64; //64Byte
-uint64_t len = size / sizeof(uint64_t);
-
-void read_dram(int _size) {
-
-}
+char *dram_addr = NULL, *pm_addr = NULL, *ssd_addr = NULL, *content = NULL;
+uint64_t *start_index = NULL;
+uint64_t start_index_len = 2 << 20;
 
 void init_addr() {
-    int pm_fd = open(pm_file.c_str(), O_CREAT | O_RDWR, 0644);
-    int ssd_fd = open(ssd_file.c_str(), O_CREAT | O_RDWR, 0644);
+    uint64_t pm_fd = open(pm_file.c_str(), O_CREAT | O_RDWR, 0644);
+    uint64_t ssd_fd = open(ssd_file.c_str(), O_CREAT | O_RDWR, 0644);
 #ifdef __linux__
     if (posix_fallocate(pm_fd, 0, ALLOC_SIZE) < 0){
         puts("pm fallocate fail\n");
@@ -63,15 +73,60 @@ void init_addr() {
         exit(-1);
     }
 #endif
-    dram_addr = (char *)malloc(ALLOC_SIZE);
+    dram_addr = (char *) malloc(ALLOC_SIZE);
     pm_addr = (char *) mmap(NULL, ALLOC_SIZE, PROT_READ | PROT_WRITE, MAP_SYNC | MAP_SHARED_VALIDATE, pm_fd, 0);
     ssd_addr = (char *) mmap(NULL, ALLOC_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, ssd_fd, 0);
     close(pm_fd);
     close(ssd_fd);
 }
 
+void create_index() {
+    start_index = new uint64_t[start_index_len];
+    rng r;
+    rng_init(&r, 1, 2);
+    uint64_t right_limit = ALLOC_SIZE - object_size;
+    for (int i = 0; i < start_index_len; ++i) {
+        start_index[i] = rng_next(&r) % right_limit;
+    }
+}
+
+uint64_t read_dram(uint64_t _size) {
+
+}
+
+uint64_t write_dram(uint64_t _size) {
+    uint64_t total_delay = 0;
+    struct timespec t1, t2;
+    for (int i = 0; i < start_index_len; ++i) {
+        int offset = start_index[i];
+        clock_gettime(CLOCK_REALTIME, &t1);
+        strcpy(dram_addr + offset, content);
+        clock_gettime(CLOCK_REALTIME, &t2);
+        total_delay += (uint64_t) (t2.tv_sec - t1.tv_sec) * 1000000000LL + (t2.tv_nsec - t1.tv_nsec);
+    }
+    return total_delay;
+}
+
+
+void benchmark() {
+    uint64_t read_dram_latency = 0, write_dram_latency = 0;
+    uint64_t read_pm_latency = 0, write_pm_latency = 0;
+    uint64_t read_ssd_latency = 0, write_ssd_latency = 0;
+    content = new char[object_size + 1];
+    for (int i = 0; i < object_size; ++i) {
+        content[i] = 'a' + (i % 26);
+    }
+    content[object_size - 1] = '\0';
+    create_index();
+
+    write_dram_latency = write_dram(object_size);
+
+    cout << write_dram_latency << endl;
+}
+
 int main() {
     init_addr();
+    benchmark();
 
     return 0;
 }
